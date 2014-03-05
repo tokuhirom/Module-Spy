f06491d0ce25df5234e75a230a872eaa13190592
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-03-05 09:40:23 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test::Spy - It's new $module
+
+# SYNOPSIS
+
+    use Test::Spy;
+
+# DESCRIPTION
+
+Test::Spy is ...
+
+# LICENSE
+
+Copyright (C) Tokuhiro Matsuno.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Tokuhiro Matsuno <tokuhirom@gmail.com>
---
+++ lib/Test/Spy.pm
@@ -0,0 +1,39 @@
+package Test::Spy;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::Spy - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::Spy;
+
+=head1 DESCRIPTION
+
+Test::Spy is ...
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom@gmail.comE<gt>
+
+=cut
+
