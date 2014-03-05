f5c09e6aa696decbbae23bd8955b83cdfc286753
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-03-05 11:13:43 +0900

    Implemented

--- lib/Test/Spy.pm
+++ lib/Test/Spy.pm
@@ -2,10 +2,144 @@ package Test::Spy;
 use 5.008005;
 use strict;
 use warnings;
+use Scalar::Util ();
+use Class::Monadic;
 
 our $VERSION = "0.01";
 
+use parent qw(Exporter);
 
+our @EXPORT = qw(spy);
+
+sub spy {
+    my ($stuff, $method) = @_;
+
+    if (Scalar::Util::blessed($stuff)) {
+        Test::Spy::Object->new($stuff, $method);
+    } else {
+        Test::Spy::Class->new($stuff, $method);
+    }
+}
+
+package Test::Spy::Base;
+
+sub stuff { shift->{stuff} }
+sub method { shift->{method} }
+
+sub called {
+    my $self = shift;
+    $self->{spy}->called;
+}
+
+sub returns {
+    my $self = shift;
+    $self->{spy}->returns(@_);
+    return $self;
+}
+
+package Test::Spy::Object;
+our @ISA=('Test::Spy::Base');
+
+my $SINGLETON_ID = 0;
+
+sub new {
+    my $class = shift;
+    my ($stuff, $method) = @_;
+
+    my $self = bless { stuff => $stuff, method => $method }, $class;
+
+    my $orig = $self->stuff->can($self->method)
+        or die "Missing $method";
+    $self->{orig} = $orig;
+
+    my $spy = Test::Spy::Sub->new($orig);
+    $self->{spy} = $spy;
+
+    {
+        no strict 'refs';
+        no warnings 'redefine';
+
+        my $klass = "Test::Spy::Singleton" . $SINGLETON_ID++;
+        unshift @{"${klass}::ISA"}, ref($stuff);
+        *{"${klass}::${method}"} = $spy;
+        bless $stuff, $klass; # rebless
+    }
+
+    return $self;
+}
+
+package Test::Spy::Class;
+our @ISA=('Test::Spy::Base');
+
+sub new {
+    my $class = shift;
+    my ($stuff, $method) = @_;
+
+    my $self = bless { stuff => $stuff, method => $method }, $class;
+
+    my $orig = $self->stuff->can($self->method)
+        or die "Missing $method";
+    $self->{orig} = $orig;
+
+    my $spy = Test::Spy::Sub->new($orig);
+    $self->{spy} = $spy;
+
+    {
+        no strict 'refs';
+        no warnings 'redefine';
+        *{$self->stuff . '::' . $self->method} = $spy;
+    }
+
+    return $self;
+}
+
+sub DESTROY {
+    my $self = shift;
+    my $stuff = $self->{stuff};
+    my $method = $self->{method};
+    my $orig = $self->{orig};
+
+    no strict 'refs';
+    no warnings 'redefine';
+    *{"${stuff}::${method}"} = $orig;
+}
+
+package Test::Spy::Sub;
+use Scalar::Util qw(refaddr);
+
+# inside-out
+our %COUNTER;
+our %RETURNS;
+
+sub new {
+    my ($class, $orig) = @_;
+
+    my $body;
+
+    my $code = sub { goto $body };
+    $body = sub {
+        $COUNTER{refaddr($code)}++;
+
+        if (exists $RETURNS{refaddr($code)}) {
+            return $RETURNS{refaddr($code)};
+        }
+
+        goto $orig;
+    };
+
+    my $self = bless $code, $class;
+    return $self;
+}
+
+sub called {
+    my $self = shift;
+    !!$COUNTER{refaddr($self)};
+}
+
+sub returns {
+    my ($self, $value) = @_;
+    $RETURNS{refaddr($self)} = $value;
+}
 
 1;
 __END__
---
+++ t/01_simple.t
@@ -0,0 +1,103 @@
+use strict;
+use warnings;
+use utf8;
+use Test::More;
+use Test::Spy;
+use Scalar::Util qw(refaddr);
+
+{
+    package X;
+    sub new { bless {}, shift }
+    sub y { 'yyy' }
+}
+
+subtest 'Spy class method', sub {
+    subtest 'Not called yet', sub {
+        # Given set spy
+        my $spy = spy('X', 'y');
+
+        # Then, it's not called
+        ok !$spy->called;
+    };
+
+    subtest 'Called', sub {
+        # Given set spy
+        my $spy = spy('X', 'y');
+
+        # When call the method
+        X->y;
+
+        # Then, it's called.
+        ok $spy->called;
+    };
+
+    subtest 'Restored', sub {
+        {
+            # Given set spy
+            my $spy = spy('X', 'y');
+        }
+
+        # When it's out-scoped
+
+        # Then, it's restored
+        is ref(X->can('y')), 'CODE';
+    };
+
+    subtest 'Stub-out by value', sub {
+        # Given set spy
+        my $spy = spy('X', 'y');
+
+        # When set return value as 3
+        is refaddr($spy->returns(3)), refaddr($spy);
+
+        # Then return value is 3
+        is(X->y, 3);
+    };
+};
+
+subtest 'Spy instance method', sub {
+    subtest 'Not called yet', sub {
+        # Given object
+        my $obj = X->new;
+
+        # Given set spy
+        my $spy = spy($obj, 'y');
+
+        # Then, it's not called
+        ok !$spy->called;
+    };
+
+    subtest 'Called', sub {
+        # Given object
+        my $obj = X->new;
+
+        # Given set spy
+        my $spy = spy($obj, 'y');
+
+        # When call the method
+        is $obj->y, 'yyy';
+
+        # Then, it's called.
+        ok $spy->called;
+    };
+
+    subtest "It's not affected for another object", sub {
+        # Given object
+        my $obj = X->new;
+
+        # Given another object
+        my $another_obj = X->new;
+
+        # Given set spy
+        my $spy = spy($obj, 'y');
+
+        # Then, $obj was spyed
+        is ref($obj->can('y')), 'Test::Spy::Sub';
+
+        # Then, but $another_obj was *not* spyed
+        is ref($another_obj->can('y')), 'CODE';
+    };
+};
+
+done_testing;
+
