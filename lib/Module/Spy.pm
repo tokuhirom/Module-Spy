800eb2d0e1fd87a37ca2191d3a359ec3bc7433d2
aereal <aereal@aereal.org>
2017-01-18 15:37:06 +0900

    Add a document about and_call_through

--- lib/Module/Spy.pm
+++ lib/Module/Spy.pm
@@ -312,6 +312,12 @@ Stub the method's return value as C<$value>.
 
 Returns C<<$spy>> itself for method chaining.
 
+=item C<< $spy->and_call_through() : Module::Spy::Base >>
+
+Do not stub the method's return value, calls original implementation.
+
+Returns C<<$spy>> itself for method chaining.
+
 =item C<< $spy->calls_any() : Bool >>
 
 Returns false if the spy has not been called at all, and then true once at least one call happens.
